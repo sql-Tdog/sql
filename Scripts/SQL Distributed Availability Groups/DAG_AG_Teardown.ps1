ca443146c760496f72e0d50d33c82a8e9efa6430
Tanya <tanyanik1210@gmail.com>
2025-09-03 10:45:46 -0700

    Update DAG_AG_Teardown.ps1

--- Scripts/SQL Distributed Availability Groups/DAG_AG_Teardown.ps1
+++ Scripts/SQL Distributed Availability Groups/DAG_AG_Teardown.ps1
@@ -1,55 +1,19 @@
 <####
-Script to tear down the AGs and the DAG
+Script to tear down the DAG and the secondary AG of the DAG 
 ####>
 
-$AG1=""
-$AG2=""
-$AG3=""
+$List1 = "" #listener of primary AG of the DAG
+$List2 = "" #listener secondary AG of the DAG
+$DAGname = ""  #DAG to tear down 
+$AG = ""  #AG to tear down 
 
-$AG1acct="$"
-$AG2acct="$"
-
-$inst1=""
-$inst2=""
-$inst3=""
-$inst4=""
-$inst5=""
-$inst6=""
-
-$List1=""
-$List2=""
-$List3=""
-
-$dbshare="\\xxx\TempShare"
-$dbshare2="\\xxx\TempShare"
-$DAGname=""
-$DAGname2=""
-
-$AD=$Env:userdomain
-$FQDN=$env:USERDNSDOMAIN
-$db="databaseName"
-
-#tear down DAG2 and AG3:
-$Query="DROP AVAILABILITY GROUP [$DAGname2]"
-Invoke-Sqlcmd -ServerInstance $List2 -Query $Query -TrustServerCertificate
-Invoke-Sqlcmd -ServerInstance $List3 -Query $Query -TrustServerCertificate
-
-$Query="DROP AVAILABILITY GROUP [$AG3]"
-Invoke-Sqlcmd -ServerInstance $List3 -Query $Query -TrustServerCertificate
-
-#tear down DAG1 and AG2
+ 
 $Query="DROP AVAILABILITY GROUP [$DAGname]"
-Invoke-Sqlcmd -ServerInstance $List1 -Query $Query -TrustServerCertificate
-Invoke-Sqlcmd -ServerInstance $List2 -Query $Query -TrustServerCertificate
+Invoke-Sqlcmd -ServerInstance $List2 -Query $Query 
+Invoke-Sqlcmd -ServerInstance $List1 -Query $Query 
 
+$Query="DROP AVAILABILITY GROUP [$AG]"
+Invoke-Sqlcmd -ServerInstance $List2 -Query $Query 
 
-$Query="ALTER AVAILABILITY GROUP [$AG2] REMOVE DATABASE $db"
-Invoke-Sqlcmd -ServerInstance $List2 -Query $Query -TrustServerCertificate
-
-$Query="DROP DATABASE $db"
-Invoke-Sqlcmd -ServerInstance $inst3 -Query $Query -TrustServerCertificate
-Invoke-Sqlcmd -ServerInstance $inst4 -Query $Query -TrustServerCertificate
-
-
-$Query="DROP AVAILABILITY GROUP [$AG2]"
-Invoke-Sqlcmd -ServerInstance $inst3 -Query $Query -TrustServerCertificate
+#the above query will error out because SQL Server won't be able to confirm that the AG was dropped since
+#it will be trying to connect to the listener that will be dropped
